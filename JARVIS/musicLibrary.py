3ad97d294279d46f133281252baae96ce3389712
Bhabani Sankar Jana <155243358+bsjana007@users.noreply.github.com>
2025-10-07 20:38:09 +0530

    Add files via upload

---
+++ JARVIS/musicLibrary.py
@@ -0,0 +1,15 @@
+music = {
+    "stealth" : "https://www.youtube.com/watch?v=U47Tr9BB_wE&list=RDU47Tr9BB_wE&start_radio=1",
+    "a thousand years" : "https://www.youtube.com/watch?v=NZGHXy1IAHM",
+    "faded" : "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "memories" : "https://www.youtube.com/watch?v=SlPhMPnQ58k&list=RDSlPhMPnQ58k&start_radio=1",
+    "somewhere only we know" : "https://www.youtube.com/watch?v=O-zpOMYRi0w",
+    "let her go" : "https://www.youtube.com/watch?v=RBumgq5yVrA",
+    "perfect" : "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "7 years" : "https://www.youtube.com/watch?v=LHCob76kigA",
+    "game of thrones theme" : "https://www.youtube.com/watch?v=s7L2PVdrb_8",
+    "zamana lage" : "https://www.youtube.com/watch?v=zHl_ZKq0NNs&list=RDzHl_ZKq0NNs&start_radio=1",
+    "bin tere" : "https://www.youtube.com/watch?v=pGuc4hPhiKw&list=RDzHl_ZKq0NNs&index=3"
+}
+
+#https://www.youtube.com/watch?v=TZE9gVF1QbA
